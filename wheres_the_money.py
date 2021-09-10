b86072a820688412b98bb41b17d975b724920f7e
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-09 22:41:56 -0700

    Update wheres_the_money.py

--- wheres_the_money.py
+++ wheres_the_money.py
@@ -16,3 +16,19 @@ c=int(input('What do you spend on bills monthly?'))
 d=int(input('What are your weekly grocery/food expenses?'))
 
 e=int(input('How much do you spend on travel annually?'))
+      
+      
+      
+      
+      
+      
+print('------------------------------------------------------------------')
+print('See the financial breakdown below, based on a salary of $'+a)
+print('------------------------------------------------------------------')
+print('| mortgage/rent | $  12,000.00 |  24.0% | ########################')
+print('|         bills | $   4,800.00 |   9.6% | #########')
+print('|          food | $  10,400.00 |  20.8% | ####################')
+print('|        travel | $   2,500.00 |   5.0% | #####')
+print('|           tax | $  10,000.00 |  20.0% | ####################')
+print('|         extra | $  10,300.00 |  20.6% | ####################')
+print('------------------------------------------------------------------
