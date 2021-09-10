4530926e62cd79533a39b51b97b8da35cbb62364
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-09 22:36:39 -0700

    Update wheres_the_money.py

--- wheres_the_money.py
+++ wheres_the_money.py
@@ -4,3 +4,15 @@
 ### Description: Describe your program with one
 ###              or more sentences of text.
 ###
+
+print('-----------------------------')
+print('----- WHERE'S THE MONEY -----')
+print('-----------------------------')
+a=int(input('What is your annual salary?'))
+
+b=int(input('How much is your monthly mortgage or rent?'))
+c=int(input('What do you spend on bills monthly?'))
+
+d=int(input('What are your weekly grocery/food expenses?'))
+
+e=int(input('How much do you spend on travel annually?'))
