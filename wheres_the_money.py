220a0a43c7775cbeacd242c4d9769b1d634056de
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-15 13:24:47 -0700

    Update wheres_the_money.py

--- wheres_the_money.py
+++ wheres_the_money.py
@@ -9,13 +9,32 @@ print('-----------------------------')
 print('----- WHERE\'S THE MONEY -----')
 print('-----------------------------')
 a=int(input('What is your annual salary?\n'))
+if type(a)!=int
+    print('Must enter positive integer for saraly.')
+elif a<=0
+    print('Must enter positive integer for saraly.')
 
 b=int(input('How much is your monthly mortgage or rent?\n'))
+if type(b)!=int
+    print('Must enter positive integer for rent.')
+elif b<=0
+    print('Must enter positive integer for rent.)
 c=int(input('What do you spend on bills monthly?\n'))
-
+if type(c)!=int
+    print('Must enter positive integer for bills.')
+elif c<=0
+    print('Must enter positive integer for bills.')
 d=int(input('What are your weekly grocery/food expenses?\n'))
+if type(d)!=int
+    print('Must enter positive integer for food.')
+elif d<=0
+    print('Must enter positive integer for food.')
 
 e=int(input('How much do you spend on travel annually?\n'))
+if type(e)!=int
+    print('Must enter positive integer for travel annually.')
+elif e<=0
+    print('Must enter positive integer for travel annually.')
 print(' ')
 if a<=15000:
     f=0.1
