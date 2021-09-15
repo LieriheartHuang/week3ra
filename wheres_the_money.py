fb457842db8e9d5e5a7f46ea9c9ba826a6da8948
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-14 19:39:05 -0700

    Update wheres_the_money.py

--- wheres_the_money.py
+++ wheres_the_money.py
@@ -32,20 +32,18 @@ h=12*b
       
       
       
-      
-      
-      
 print('------------------------------------------------------------------')
-print('See the financial breakdown below, based on a salary of $'+a)
+print('See the financial breakdown below, based on a salary of $'+str(a))
 print('------------------------------------------------------------------')
-print('| mortgage/rent | $  '+"{:>10d}".format(h)+ '|'+"{:3.1%}"format(h/a)+' | '+int(h/a)*'#')
-print('|         bills | $  '+"{:>10d}".format(12*c)+ '|'+"{:3.1%}"format(12*c/a)+' | '+int(12*c/a)*'#')
-print('|          food | $  '+"{:>10d}".format(52*d)+ '|'+"{:3.1%}"format(52*d/a)+' | '+int(52*d/a)*'#')
-print('|        travel | $  '+"{:>10d}".format(e)+ '|'+"{:3.1%}"format(e/a)+' | '+int(e/a)*'#')
-print('|           tax | $  '+"{:>10d}".format(f*a)+ '|'+"{:3.1%}"format(f)+' | '+int(f)*'#')
-print('|         extra | $  '+"{:>10d}".format(g)+ '|'+"{:3.1%}"format(g/a)+' | '+int(g/a)*'#')
+print('| mortgage/rent | $  '+"{:>10d}".format(h)+ '|'+"{:3.1%}".format(h/a)+' | '+int(h/a)*'#')
+print('|         bills | $  '+"{:>10d}".format(12*c)+ '|'+"{:3.1%}".format(12*c/a)+' | '+int(12*c/a)*'#')
+print('|          food | $  '+"{:>10d}".format(52*d)+ '|'+"{:3.1%}".format(52*d/a)+' | '+int(52*d/a)*'#')
+print('|        travel | $  '+"{:>10d}".format(e)+ '|'+"{:3.1%}".format(e/a)+' | '+int(e/a)*'#')
+print('|           tax | $  '+"{:>10d}".format(int(f*a))+ '|'+"{:3.1%}".format(f)+' | '+int(f)*'#')
+print('|         extra | $  '+"{:>10d}".format(int(g))+ '|'+"{:3.1%}".format(g/a)+' | '+int(g/a)*'#')
 print('------------------------------------------------------------------')
-if g<=0
+if g<=0:
     print('>>> WARNING: DEFICIT <<<')
-if a>75000
+if a>75000:
     print('>>> TAX LIMIT REACHED <<<')
+
