ea3c1452a092f8471b6213bf438ed658a28633d5
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-15 17:28:15 -0700

    Update wheres_the_money.py

--- wheres_the_money.py
+++ wheres_the_money.py
@@ -8,29 +8,29 @@
 print('-----------------------------')
 print('----- WHERE\'S THE MONEY -----')
 print('-----------------------------')
-a=int(input('What is your annual salary?\n'))
+a=input('What is your annual salary?\n')
 if (not isinstance(a,int)):
     print('Must enter positive integer for saraly.')
 if a<=0:
     print('Must enter positive integer for saraly.')
 
-b=int(input('How much is your monthly mortgage or rent?\n'))
+b=input('How much is your monthly mortgage or rent?\n')
 if (not isinstance(b,int)):
     print('Must enter positive integer for rent.')
 if b<=0:
     print('Must enter positive integer for rent.')
-c=int(input('What do you spend on bills monthly?\n'))
+c=input('What do you spend on bills monthly?\n')
 if (not isinstance(c,int)):
     print('Must enter positive integer for bills.')
 if c<=0:
     print('Must enter positive integer for bills.')
-d=int(input('What are your weekly grocery/food expenses?\n'))
+d=input('What are your weekly grocery/food expenses?\n')
 if (not isinstance(d,int)):
     print('Must enter positive integer for food.')
 if d<=0:
     print('Must enter positive integer for food.')
 
-e=int(input('How much do you spend on travel annually?\n'))
+e=input('How much do you spend on travel annually?\n')
 if (not isinstance(e,int)):
     print('Must enter positive integer for travel annually.')
 if e<=0:
