d9ad5a159851d63ed4de88925a3e5cff21bb40c2
LieriheartHuang <89817234+LieriheartHuang@users.noreply.github.com>
2021-09-29 11:03:26 -0700

    Update prep13.py

--- prep13.py
+++ prep13.py
@@ -1,1 +1,6 @@
-
+def count_characters(str_1, str_2, str_3):
+	cnt = 0
+	for c in str_1:
+		if c == str_2[0] or c == str_3[0]:
+			cnt += 1
+	print("'%s' and '%s' appeared %d times in the string '%s'" % (str_2, str_3, cnt, str_1))
